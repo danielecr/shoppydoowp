fc6893d648f59900cc278b86aec9fb615fa05efa
Daniele Cruciani <daniele@smartango.com>
2015-04-17 12:35:28 +0200

    initial commit

---
+++ README.txt
@@ -0,0 +1,39 @@
+=== bbplanet feed ===
+Contributors: danielecr
+Donate link: http://www.smartango.com/
+Tags: appartment, receiptive, feed, link
+Stable tag: 1.0
+Requires at least: 3.1
+Tested up to: 3.8
+License: GPLv2 or later
+License URI: http://www.gnu.org/licenses/gpl-2.0.html
+
+== Description ==
+
+Display a list of available (in bbplanet) receiptive structure
+(hotel,b&b, appartments, residence, agriturism) in an article.
+
+Insert the right tag in the article and with this plugin
+activated it will be listed all structure available from
+bbplanet.net feed 
+
+== Installation ==
+
+Copy this folder in directory wp-content/plugins/ of your wordpress
+
+Make sure the script can write the cache directory, have a look
+via ftp, in doubt set it to 777
+
+== Upgrade ==
+
+I am experiencing some problem, deactivate and reactivate the module
+should fix those. Be carefull, just make a backup of table data
+(prefix_category_ext_feed), deactivate plugin, then reactivate.
+Thus if data about feed is not available anymore, just replace
+the table.
+
+
+== Changelog ==
+
+= 0.1 =
+* initial release (only readme)
---
+++ bbplanetwp.php
@@ -0,0 +1,211 @@
+<?php
+/* 
+   Plugin Name: BBPlanet.net Feed injector for Wordpress
+   Plugin URI: http://www.smartango.com/
+   Description: Inject BBPlanet hotels et all into wp articles
+   Author: Daniele Cruciani
+   Version: 0.1
+   Author URI: http://www.smartango.com 
+*/
+
+
+add_filter('the_content', 'bbplanet_parse');
+
+function bbplanet_parse($content='')
+{
+	if(preg_match('/\[\[bbplanet\:([^|]+)\|?(cat:(.*))?\]\]/',$content,$matches)) {
+		$whole = $matches[0];
+		$city = $matches[1];
+		$cat = null;
+		if(isset($matches[3])) {
+			$cat = $matches[3];
+		}
+		//return preg_replace("/".preg_quote($whole)."/",$city,$content);
+		$stru = new bbPlanetStru();
+		$calcString = $stru->getAll($city,$cat);
+		return preg_replace("/".preg_quote($whole)."/",$calcString,$content);
+	}
+}
+
+function bbplanet_get_structures($localita, $types = NULL)
+{
+}
+
+class bbParsedStru
+{
+	var $elements = array(
+		'IDStruttura',
+		'tipologia',
+		'tipologiaestesa',
+		'nomestruttura',
+		'stelle',
+		'zona',
+		'localita',
+		'citta',
+		'provincia',
+		'regione',
+		'immagine',
+		'descrizione',
+		'linkstruttura',
+		'xmlstruttura',
+		'latitudine',
+		'longitudine',
+		'npostiletto',
+		'nfeedback',
+		'datapub',
+		);
+
+	var $structures = array();
+	var $iC =0;
+
+	function __construct($xml = NULL)
+	{
+		if($xml) {
+			$this->parseXml($xml);
+		}
+	}
+	function add($xml = NULL)
+	{
+		if($xml) {
+			$this->parseXml($xml);
+		}
+	}
+
+	function reset()
+	{
+		$this->iC =0;
+	}
+
+	function getStruct()
+	{
+		if($this->iC>count($this->structures)) return false;
+		return $this->structures[$this->iC++];
+	}
+
+	function parseXml($xml)
+	{
+		$ff = count($this->structures);
+		foreach($this->elements as $element) {
+			$delta=0;
+			foreach($xml->{$element} as $k => $el) {
+				if(count($this->structures) == $ff + $delta ) {
+					$this->structures[$ff+$delta] = new StdClass;
+				}
+				$this->structures[$ff+$delta]->{$element} = (string) $el;
+				$delta++;
+			}
+		}
+	}
+}
+
+class bbPlanetStru
+{
+	var $stypes = array('bb' => 'BB',
+			    'ho' => 'Albergo',
+			    'ap' => 'Appartamento',
+			    'at' => 'Agriturismo',
+			    're' => 'Residence',
+		);
+	
+	var $baseStUrl = 'http://xml.bbplanet.net/xml/strutture.xml';
+	
+	var $ida;
+	var $lang = 'IT';
+	
+	public function __construct($ida='10463')
+	{
+		$this->ida = $ida;
+	}
+	
+	private function getTheXml($params)
+	{
+		$url = $this->buildStUrl($params);
+		$xml = simplexml_load_file($url);
+		return $xml;
+	}
+
+	private function getNStrutture($xml)
+	{
+		foreach($xml->attributes() as $k=>$v) {
+			if($k == 'nstrutture') {
+				return $v;
+			}
+		}
+		return 0;
+	}
+
+	public function getStructs($localita, $type = NULL)
+	{
+		$basePar = array('l'=>$localita,'t'=>$type);
+		$xml = $this->getTheXml($basePar);
+		$nstrutture = $this->getNStrutture($xml);
+		$parsed = new bbParsedStru($xml);
+
+		if($nstrutture>10) {
+			$p = 1;
+			do {
+				$basePar['p'] = $p;
+				$xml = $this->getTheXml($basePar);
+				$parsed->add($xml);
+				$p++;
+			}while($p*10<$nstrutture);
+		}
+		//print_r($parsed);
+		return $parsed;
+	}
+		
+	private function buildStUrl($params)
+	{
+		$qp = array('ida'=>$this->ida,
+			    'localita'=>$params['l'],
+			    'lang' => $this->lang,
+			);
+		if(isset($params['t']) && $params['t'] != NULL) {
+			$qs['tipologia'] = $params['t'];
+		}
+		if(isset($params['p']) && $params['p'] != NULL) {
+			$qs['p'] = $params['p'];
+		}
+		$qs = http_build_query($qp);
+		return $this->baseStUrl . '?' .$qs;
+	}
+
+	function getAll($city,$cat)
+	{
+		$parsed = $this->getStructs($city, $cat);
+		$formatter = new bbPlanetFormatter();
+		return $formatter->formatAll($parsed->structures);
+	}
+}
+
+class bbPlanetFormatter
+{
+	var $begin = '<ul>';
+	var $end = '</ul>';
+	var $element = "<li>A [[citta]] esiste un hotel
+<strong>[[nomestruttura]]<br />
+<p>[[descrizione]]</p>
+<p>Vacci tu a <a href=\"[[linkstruttura]]\">[[nomestruttura]]</a></p></li>";
+	function __costruct()
+	{
+		
+	}
+
+	function formatStruct($struct)
+	{
+		$orig = $this->element;
+		foreach($struct as $k => $v) {
+			$orig = preg_replace('/\[\['.preg_quote($k).'\]\]/',$v,$orig);
+		}
+		return $orig;
+	}
+
+	function formatAll($structures)
+	{
+		$content = '';
+		foreach($structures as $stru) {
+			$content .= $this->formatStruct($stru);
+		}
+		return $this->begin . $content . $this->end;
+	}
+}
